a242d7c21d8bd0f41ca529f045f7534ab4b77b58
osamtimizer <eaten.jb@gmail.com>
2022-05-27 18:02:38 +0900

    unused file

--- bin/dart_http_server_sample.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_http_server_sample/dart_http_server_sample.dart' as dart_http_server_sample;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_http_server_sample.calculate()}!');
-}
--- test/dart_http_server_sample_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_http_server_sample/dart_http_server_sample.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
