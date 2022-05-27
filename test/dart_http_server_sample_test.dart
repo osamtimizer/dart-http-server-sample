0a86427d524b3dae5736203fd24b7ee57a32719a
osamtimizer <eaten.jb@gmail.com>
2022-05-27 17:55:49 +0900

    create dart project

---
+++ test/dart_http_server_sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_http_server_sample/dart_http_server_sample.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
